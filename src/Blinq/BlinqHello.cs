d4fb1137414dd84b7e02ff0ba6e19d9a116ad857
Krzysztof Begiedza <krzysztof.begiedza@gmail.com>
2020-05-31 13:31:32 +0200

    Init

---
+++ src/Blinq/BlinqHello.cs
@@ -0,0 +1,10 @@
+﻿// <copyright file="BlinqHello.cs">
+// Copyright (c) Krzysztof Begiedza. All rights reserved.
+// </copyright>
+
+namespace Blinq
+{
+    public class BlinqHello
+    {
+    }
+}
---
+++ tests/Blinq.Tests/BlinqHelloTests.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace Blinq.Tests
+{
+    public class BlinqHelloTests
+    {
+        [Fact]
+        public void Should_Pass()
+        {
+        }
+
+        [Fact]
+        public void Should_Fail()
+        {
+        }
+    }
+}
